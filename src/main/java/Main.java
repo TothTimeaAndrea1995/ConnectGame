87404690a9fa91dfe3da8bd987b349b6ad818bd3
TothTimeaAndrea1995 <115030331+TothTimeaAndrea1995@users.noreply.github.com>
2025-01-08 14:50:03 +0100

    main.java néhány módosítása

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
 public class Main {
+    // játéktábla
+    private int[][] gameBoard = new int[7][6];
     public static void main(String[] args) {
        // System.out.println("Hello World!");
-
+        // Main main = new mainClass();
     }
+
+    // public mainClass(){
+    //     int counter = setPlayer();
+    //     boolean gameCont = true;
+    //     while(gameCont){
+    //         int[][] board = getGameBoard();
+    //         int move = getMove(counter);
+    //         if(move == 10){
+    //             System.out.println("Érvénytelen oszlopszám!");
+    //         }else{
+    //             int row = getRow(move);
+    //             if(row == 10){
+    //                 System.out.println("Érvénytelen lépészám!");
+    //             }
+    //         }
+    //     }        
 }
