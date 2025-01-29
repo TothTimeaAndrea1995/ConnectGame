868f3d8c77021488a0408afb6ceac35b172c56c4
TothTimeaAndrea1995 <115030331+TothTimeaAndrea1995@users.noreply.github.com>
2025-01-29 22:33:10 +0100

    új static bevezetése

--- src/main/java/GameModel/GameBoard.java
+++ src/main/java/GameModel/GameBoard.java
@@ -1,5 +1,7 @@
 package GameModel;
 
+//import java.util.Arrays;
+
 public class GameBoard {
     // Mivel nem szeretném módosítani a tábla sorait és oszlopait,
     // ezért használtam private static final-t
@@ -7,32 +9,14 @@ public class GameBoard {
     private static final int R = 6;  // R: row - sorok száma
     private static final int C = 7; // C: column - oszlopok száma
 
-    // for használatával meghatározzuk a táblát
-    public GameBoard() {
-        board = new char[R][C];
-        for(int i=0; i<R; i++) {
-            for(int j=0; j<C; j++) {
-                board[i][j] = '.';
-            }
-        }
-    }
-
-    // Szintén a for használatával megjelenítem a képernyőt
-    public void printBoard() {
-        for (int i = 0; i < R; i++) {
-            for (int j = 0; j < C; j++) {
-                System.out.println(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+    public static int getR() {return R;}
+    public static int getC() {return C;}
+    public char[][] getGameBoard() {return board;}
 
     // Egy boolean ellenőrzi, hogy a mozdulat érvényes-e, és ha igen,
     // letesz egy adott szimbólumot
     public boolean placeCoin(int col, char symbol) {
-        if (col < 0 || col >= C || board[0][col] != '.') {
-            return false; // false értéket ad vissza, ha érvénytelen a mozdulat vagy tele van a sor
-        }
+        if (col < 0 || col >= C || board[0][col] != '.') return false; // false értéket ad vissza, ha érvénytelen a mozdulat vagy tele van a sor
         for (int row = R - 1; row >= 0 ; row--) {
             if (board[row][col] == '.') {
                 board[row][col] = symbol;
@@ -42,6 +26,11 @@ public class GameBoard {
         return false;
     }
 
-
+    public void printBoard() {
+        for (char[] row : board) {
+            System.out.println(String.valueOf(row));
+        }
+        System.out.println("0123456"); // Oszlop indexek
+    }
 
 }
