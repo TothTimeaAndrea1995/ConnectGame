220f8ed525f1914b853e154048afb01ab774dbba
TothTimeaAndrea1995 <115030331+TothTimeaAndrea1995@users.noreply.github.com>
2025-01-29 22:34:25 +0100

    játék elindításához szükséges parancs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,8 @@
-public class Main {
-    // játéktábla
-    private int[][] gameBoard = new int[7][6];
-    public static void main(String[] args) {
-       //System.out.println("Hello World!");
-        // Main main = new mainClass();
-    }
+import GameModel.setGame;
 
-    // public mainClass(){
-    //     int counter = setPlayer();
-    //     boolean gameCont = true;
-    //     while(gameCont){
-    //         int[][] board = getGameBoard();
-    //         int move = getMove(counter);
-    //         if(move == 10){
-    //             System.out.println("Érvénytelen oszlopszám!");
-    //         }else{
-    //             int row = getRow(move);
-    //             if(row == 10){
-    //                 System.out.println("Érvénytelen lépészám!");
-    //             }
-    //         }
-    //     }
+public class Main {
+    public static void main(String[] args) {
+        setGame game = new setGame();
+        game.startGame();
+    }
 }
